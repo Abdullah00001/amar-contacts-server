db64b5bd5b991470b26e0d205800d70f20575868
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury02@gmail.com>
2025-04-26 16:09:55 +0600

    config(test-docker): configured tsconfig and docker and docker compose files

--- __tests__/user/integration/user.repositories.test.ts
+++ __tests__/user/integration/user.repositories.test.ts
@@ -1,4 +1,4 @@
-
+// const {}=UserMod
 describe('Integration Test - user.repositories.test.ts', () => {
   it('should test user.repositories.test.ts functionality', async () => {
     // Add test logic for user.repositories.test.ts
--- __tests__/user/unit/user.repositories.test.ts
+++ __tests__/user/unit/user.repositories.test.ts
@@ -1,6 +1,36 @@
+import User from '../../../src/modules/user/user.models';
+import UserRepositories from '../../../src/modules/user/user.repositories';
+const { findUserByEmailOrPhone } = UserRepositories;
+jest.mock('../../../src/modules/user/user.models');
 
-describe('Unit Test - user.repositories.test.ts', () => {
-  it('should test user.repositories.test.ts functionality', async () => {
-    // Add test logic for user.repositories.test.ts
+describe('User Repositories', () => {
+  describe('Find User By Email Or Phone', () => {
+    it('It Should Be Return Object, If The User Found With Email', async () => {
+      (User.findOne as jest.Mock).mockResolvedValue({
+        email: 'example@gmail.com',
+      });
+      const result = await findUserByEmailOrPhone('example@gmail.com');
+      expect(result).toEqual({
+        email: 'example@gmail.com',
+      });
+    });
+    it('It Should Be Return Object, If The User Found With Phone', async () => {
+      (User.findOne as jest.Mock).mockResolvedValue({
+        phone: '01937868838',
+      });
+      const result = await findUserByEmailOrPhone('01937868838');
+      expect(result).toEqual({ phone: '01937868838' });
+    });
+    it('It Should Be Return Null, If The User Not Found', async () => {
+      (User.findOne as jest.Mock).mockResolvedValue(null);
+      const result = await findUserByEmailOrPhone('thxdhxt');
+      expect(result).toBeNull();
+    });
+    it('It Should Throw Error If Something Goes Wrong', async () => {
+      (User.findOne as jest.Mock).mockRejectedValue(new Error('DB Error'));
+      await expect(findUserByEmailOrPhone('error@gmail.com')).rejects.toThrow(
+        'DB Error'
+      );
+    });
   });
 });
