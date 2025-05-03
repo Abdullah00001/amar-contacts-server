dbe6929e34848298387760e2b57f7ade9f674312
Abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-05-03 23:17:20 +0600

    feat(login):login endpoint implemented and add urlencoder and multer as middleware for accept form data

--- src/routes/v1/user.routes.ts
+++ src/routes/v1/user.routes.ts
@@ -2,12 +2,21 @@ import { Router } from 'express';
 import UserControllers from '@/modules/user/user.controllers';
 import UserMiddlewares from '@/modules/user/user.middlewares';
 
-const { isSignupUserExist, checkOtp, isUserExist } = UserMiddlewares;
-const { handleSignUp, handleVerifyUser } = UserControllers;
+const {
+  isSignupUserExist,
+  checkOtp,
+  isUserExistAndVerified,
+  checkPassword,
+  isUserExist,
+} = UserMiddlewares;
+const { handleSignUp, handleVerifyUser, handleLogin } = UserControllers;
 
 const router = Router();
 
 router.route('/auth/signup').post(isSignupUserExist, handleSignUp);
 router.route('/auth/verify').post(isUserExist, checkOtp, handleVerifyUser);
+router
+  .route('/auth/login')
+  .post(isUserExistAndVerified, checkPassword, handleLogin);
 
 export default router;
