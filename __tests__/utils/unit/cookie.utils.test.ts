32adf46f0f5bb9aa62150c995c146b67db6a1965
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-06-14 22:47:07 +0600

    test(utils): add test for CookieUtils methods

--- __tests__/utils/unit/cookie.utils.test.ts
+++ __tests__/utils/unit/cookie.utils.test.ts
@@ -1,3 +1,37 @@
 import CookieUtils from '../../../src/utils/cookie.utils';
 
-const { cookieOption,sharedCookieOption } = CookieUtils;
+const { cookieOption, sharedCookieOption } = CookieUtils;
+
+describe('CookieUtils', () => {
+  describe('cookieOption method should be take string as format "ms","s","m","h","d" and return cookie option object base on ENVIRONMENT', () => {
+    it('it should take 1ms and return cookie option with max age 1 ms', () => {
+      const result = cookieOption('1ms');
+      expect(result.maxAge).toBe(1);
+    });
+    it('it should take 1s and return cookie option with max age 1 s', () => {
+      const result = cookieOption('1s');
+      expect(result.maxAge).toBe(1 * 1000);
+    });
+    it('it should take 1m and return cookie option with max age 1 m', () => {
+      const result = cookieOption('1m');
+      expect(result.maxAge).toBe(1 * 60 * 1000);
+    });
+    it('it should take 1h and return cookie option with max age 1 h', () => {
+      const result = cookieOption('1h');
+      expect(result.maxAge).toBe(1 * 60 * 60 * 1000);
+    });
+    it('it should take 1d and return cookie option with max age 1 d', () => {
+      const result = cookieOption('1d');
+      expect(result.maxAge).toBe(1 * 24 * 60 * 60 * 1000);
+    });
+    it('it should take 1dd and throw error', () => {
+      expect(() => cookieOption('1dd')).toThrow('Invalid expiresIn format');
+    });
+  });
+  describe('sharedCookieOption should return an object for shared cookies base on ENVIRONMENT', () => {
+    it('it should be return an object', () => {
+      const result = sharedCookieOption();
+      expect(typeof result).toBe('object');
+    });
+  });
+});
