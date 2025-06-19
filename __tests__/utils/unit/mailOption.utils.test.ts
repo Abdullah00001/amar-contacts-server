7bb8de2bedb79c211d83cea945ceabcb48a7af04
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-06-19 23:35:05 +0600

    test: add test for mailOption utility

---
+++ __tests__/utils/unit/mailOption.utils.test.ts
@@ -0,0 +1,13 @@
+import mailOption from '../../../src/utils/mailOption.utils';
+import { IMailOption } from '../../../src/interfaces/mailOption.interfaces';
+
+describe('Mail Option Utility', () => {
+  it('it should be return an object', () => {
+    const result = mailOption(
+      'mock@example.com',
+      'test',
+      '<h1>hello world</h1>'
+    );
+    expect(typeof result).toBe('object');
+  });
+});
